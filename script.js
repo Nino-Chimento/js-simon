337470e07d8cce496b2d6b40b7e6fddb45cd034b
Nino Chimento <saninoc84@gmail.com>
2020-01-21 12:33:20 +0100

    creazione lista numeri random

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert("N")
+var listaNumeri = [];
+for (var i = 0; i < 5; i++) {
+  listaNumeri.push(getRandomInt(1,50))
+}
+console.log(listaNumeri);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
+}
